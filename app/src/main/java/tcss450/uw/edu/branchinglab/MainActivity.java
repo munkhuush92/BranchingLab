7802e10c38c2cd9b13465b6d8bf39829025508e7
Ian Nicholas <imnich91@gmail.com>
2017-01-13 17:08:29 -0800

    ?

---
+++ app/src/main/java/tcss450/uw/edu/branchinglab/MainActivity.java
@@ -0,0 +1,14 @@
+package tcss450.uw.edu.branchinglab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("HI");
+    }
+}
